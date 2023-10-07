403ea3c4158fc14c2c8656698c476f53b09f0f8d
mederian <ole.snekkermo@gmail.com>
2023-10-07 09:21:00 +0200

    Made some changes after understaing assignment

--- project calculator/script.js
+++ project calculator/script.js
@@ -1,3 +1,59 @@
+const displayView = document.querySelector('.display');
+const numericButtons = document.querySelectorAll(".numeric");
+const operatorButtons = document.querySelectorAll(".operator")
+const displayData = {
+    firstNum: 0,
+    hasNum: false, //if firstNum is a result of previous operation
+    operator: "none",
+    secondNum: 0,
+}
+
+
+operatorButtons.forEach(button =>{
+    button.addEventListener('click', buttonListener);
+})
+
+numericButtons.forEach(element => {
+    element.addEventListener('click', buttonListener);
+});
+
+function buttonListener(e){
+    if(e.target.classList.contains("operator")){
+        if(displayData.hasNum){
+            if(e.target.dataset.value == "="){
+                //check if we have two values and an operator
+                //then do the calculation..
+            }
+            else{
+                console.log("operator");
+                displayData.operator = e.target.dataset.value;    
+            }
+        }
+        
+    }
+    else if(e.target.classList.contains("numeric")){
+        if(!displayData.hasNum || displayData.operator == "none"){
+            if(displayData.firstNum == 0) displayData.firstNum = e.target.dataset.value; 
+            else displayData.firstNum += e.target.dataset.value;  
+            displayData.hasNum = true;  
+        }
+        else{
+            if(displayData.secondNum == 0) displayData.secondNum = e.target.dataset.value; 
+            else displayData.secondNum += e.target.dataset.value;         
+        }
+    }
+    else if(e.target.classList.contains("special")){
+        
+    }
+
+
+    const first = displayData.firstNum;
+    const op = displayData.operator;
+    const second = displayData.secondNum;
+
+    displayView.innerText =  `${first} ${op} ${second}`;    
+}
+
 function add(a,b){
     return a+b;
 }
@@ -13,11 +69,43 @@ function divide(a,b){
     return a/b;
 }
 
-let aVal = 0;
-let bVal = 0;
+
+
+
+function addnumber(number){
+    if(displayData.operator == 'undefined'){
+        //add digit to firstNum
+    }
+    else{
+        //add digit to secondNum
+    }
+}
+function addOperator(op){
+    if(displayData.operator != 'undefined') return;
+    else{
+        displayData.operator = op;
+    }
+}
+
+function updateDisplay(displayData){
+    displayView.innerText = (`${displayData.firstNum} ${displayData.operator} ${displayData.secondNum}`);
+}
+
+//value and operators
+let aVal = 0;  //The first number on the display
+let bVal = 0;  //The second number in the display
+                // result becomes the first number
+                // if typing a number after gotten an result, erase result with typed number
+                //
 let operator = "add";
 
-function operator(a, b, op){
+function addNumberToDisplay(num){
+
+}
+
+
+
+function operation(a, b, op){
     if(op == "add") return add(a,b);
     if(op == "sub") return sub(a,b);
     if(op == "multiply") return multiply(a,b);
